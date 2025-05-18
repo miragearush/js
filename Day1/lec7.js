16a2567d7e008006769478a798fbd23677fd59b4
miragearush <arush.kumar@outlook.in>
2025-05-18 17:19:45 +0530

    Added lecture 7 notes

---
+++ Day1/lec7.js
@@ -0,0 +1,27 @@
+let val=30;
+let negVal=-val;
+//console.log(negVal);//-30
+
+console.log(2**3);// power 
+// rest +,-,*,/,% are same as in  C/C++
+
+let str1="hello";
+let str2=" world";
+let str3=str1+str2;
+//console.log(str3);//hello world
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+"2");//12
+console.log("1"+2+3);//123
+console.log(1+2+"3");//33
+// string is calculated from left
+
+//postfix & prefix increment
+
+let x=10;
+let y=++x;//first increment then assigned
+x=10;
+let z=x++;//first assigned then incremented
+
+console.log([y,z,x]);
