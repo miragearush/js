ea4777708c6ba61dc83d39442d867b469dfa6c53
miragearush <arush.kumar@outlook.in>
2025-05-18 15:36:26 +0530

    added some more

--- Day1/lec4.js
+++ Day1/lec4.js
@@ -1,1 +1,11 @@
-console.log("a")
+const name="arush";
+let email="arush@mail.com";
+var age=24;
+password="mypass";
+
+console.log(name);
+console.log(email);
+console.log(age);
+console.log(password);
+
+//name="monika"; -> this will lead to error since 
